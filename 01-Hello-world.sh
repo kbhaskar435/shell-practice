4903397b9e9b24bf4645aca73d36e1abf44df635
kbhaskar435 <kbhaskar435@gmail.com>
2025-05-22 07:43:04 +0530

    hello world started

--- 01-Hello-world.sh
+++ 01-Hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Hello, World!"
+echo "Trump: Hello  Musk"
+echo "Musk: Hello Trump"
+echo "Trump: How are you?"
+echo "Musk: I am fine, thank you."
+echo "Trump: What are you doing?"
