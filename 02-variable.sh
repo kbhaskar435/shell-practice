8de96683761a065d369130f4c2cf17c94700ceb4
kbhaskar435 <kbhaskar435@gmail.com>
2025-05-22 07:49:23 +0530

    hello world started

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1="Trump"
+person2="Musk"
+
+echo "$person1: Hello  $person2"
+echo "$person2: Hello $person1" 
+echo "$person1: How are you?"
+echo "$person2: I am fine, thank you."
+echo "$person1: What are you doing?"
