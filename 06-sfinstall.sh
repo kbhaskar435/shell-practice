c1285aeff8e0e8789a0ae8d097812fa01b6c8fa8
kbhaskar435 <kbhaskar435@gmail.com>
2025-05-22 16:08:40 +0530

    hello world started

--- 06-sfinstall.sh
+++ 06-sfinstall.sh
@@ -9,28 +9,28 @@ E="\e[0m"
 
 if [ $USERID -ne 0 ] 
 then
-    echo "$R ERROR: This script must be run as root. $E"
+    echo -e "$R ERROR: This script must be run as root. $E"
     exit 1
 else
-    echo "$G Running as root user. $E"
+    echo -e "$G Running as root user. $E"
     
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "$Y MySQL is not installed. Proceeding with installation. $E"
+    echo -e "$Y MySQL is not installed. Proceeding with installation. $E"
     dnf install mysql -y
     if [ $? -eq 0 ]
     then
-        echo "$G MySQL installation completed successfully. $E"
+        echo -e "$G MySQL installation completed successfully. $E"
     else
-        echo "$R MySQL installation failed. $E"
+        echo -e "$R MySQL installation failed. $E"
         exit 1
     fi
     
 else
-    echo "$Y MySQL is already installed. Proceeding with configuration. $E"
+    echo -e "$Y MySQL is already installed. Proceeding with configuration. $E"
 
 fi
 
